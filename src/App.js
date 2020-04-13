2604a71580cea6f602eb850de51b3986024b4927
Chris-css <christelle.savoca@gmail.com>
2020-04-13 19:43:04 +0200

    créer ContactList et afficher une liste de contacts avec map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList';
+
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Crystal Hale"
-        avatar="https://randomuser.me/api/portraits/women/91.jpg"
-        online={false}
-      />
-      <Contact
-        name="Eli Cole"
-        avatar="https://randomuser.me/api/portraits/men/15.jpg"
-        online={true}
-      />
-      <Contact
-        name="Yolanda Welch"
-        avatar="https://randomuser.me/api/portraits/women/62.jpg"
-        online={true}
-      />
+      <ContactList />
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Contact from './Contact';
+
+
+
+const users = [
+  {
+    name: 'Robert Reyes',
+    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+    online: false
+  },
+  {
+    name: 'Nellie Caldwell',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true
+  },
+  {
+    name: 'Vernon Mason',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: true
+  },
+  {
+    name: 'Erica Hunt',
+    avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+    online: false
+  },
+  {
+    name: 'Juanita Phillips',
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+    online: true
+  }
+];
+
+
+const ContactList = () => (
+    <div>
+        {users.map(item => (
+    <Contact name={item.name} avatar={item.avatar} online={item.online} key={item.name} />
+)
+        )}
+    </div>
+)
+
+
+export default ContactList;
