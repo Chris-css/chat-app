6d7e16cb57bdf3703c3808401ce8928b3f10155f
Chris-css <christelle.savoca@gmail.com>
2020-04-14 01:39:05 +0200

    quête react 05

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,43 @@ import React from 'react';
 import './Contact.css';
 import PropTypes from 'prop-types';
 
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {online: props.online,
+        };
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
+                <div>
+                    <p className="name">{this.props.name}</p>
+                    <div className="status">
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}
+                        onClick= {event => {
+                            const newOnline = !this.state.online;
+                            this.setState ({ online: newOnline });
+                        }}
+                        >
+                        </div>
+                        <p className="status-text">{this.state.online ? "online" : "offline"}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+/*className={this.state.favorite ? 'is-favorite' : ''}
+                        onClick= {event => {
+                            const newFavorite = !this.state.favorite;
+                            this.setState ({ favorite: newFavorite });
+                        }}*/
+
+
+/*
 function Contact(props) {
     return (
         <div className="Contact">
@@ -17,11 +54,10 @@ function Contact(props) {
         </div>
     );
 }
-
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
     online: PropTypes.bool.isRequired,
 }
-
+*/
 export default Contact;
